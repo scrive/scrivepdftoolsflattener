62e935e690a3b8a292d767b981ddea74895ea393
Mariusz Rak <mariusz@scrive.com>
2020-05-19 18:48:02 +0200

    Itext 7.1.7 version - just a version test

--- src/main/java/com/scrive/pdftools/tools/Flattener.java
+++ src/main/java/com/scrive/pdftools/tools/Flattener.java
@@ -1,9 +1,8 @@
 package com.scrive.pdftools.tools;
 
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.exceptions.BadPasswordException;
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.forms.PdfAcroForm;
+import com.itextpdf.kernel.pdf.*;
+
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -16,21 +15,14 @@ public class Flattener {
             System.exit(-1);
         }
         try {
-            PdfStamper stamper;
-            PdfReader reader = new PdfReader(args[0]);
-            FileOutputStream os = new FileOutputStream(args[1]);
-            try {
-                stamper = new PdfStamper(reader, os);
-            } catch (BadPasswordException e) {
-                PdfReader.unethicalreading = true;
-                stamper = new PdfStamper(reader, os);
-            }
-            stamper.setFormFlattening(true);
-            stamper.close();
+
+            PdfDocument pdfDoc = new PdfDocument(new PdfReader(args[0]), new PdfWriter(args[1]));
+            PdfAcroForm fields = PdfAcroForm.getAcroForm(pdfDoc, true);
+
+            fields.flattenFields();
+            pdfDoc.close();
             return;
-        } catch (DocumentException e) {
-            e.printStackTrace();
-            System.exit(-1);
+
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
