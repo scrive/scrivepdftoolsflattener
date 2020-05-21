9573f0bed85936b6f94369e9de32b413ffa05e13
Mariusz Rak <mariusz@scrive.com>
2020-05-21 11:55:04 +0200

    Itext 7 based version

--- src/main/java/com/scrive/pdftools/tools/Flattener.java
+++ src/main/java/com/scrive/pdftools/tools/Flattener.java
@@ -1,21 +1,25 @@
 package com.scrive.pdftools.tools;
 
 import com.itextpdf.forms.PdfAcroForm;
-import com.itextpdf.kernel.pdf.*;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.licensekey.LicenseKey;
 
-
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Flattener {
     public static void main(String[] args) {
         if (args.length != 2) {
+
             System.err.println("Usage:");
             System.err.println("    java -jar scrivepdftoolsflattener.jar input output");
             System.exit(-1);
         }
         try {
-
+            try {
+                LicenseKey.loadLicenseFile( "scrivepdftools/itextkey.xml" );
+            } catch (Exception e) {}
             PdfDocument pdfDoc = new PdfDocument(new PdfReader(args[0]), new PdfWriter(args[1]));
             PdfAcroForm fields = PdfAcroForm.getAcroForm(pdfDoc, true);
 
